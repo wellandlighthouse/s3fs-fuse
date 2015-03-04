8623a8463ed8c2b45409c22696a29af25032a0d2
Louis McCoy <louie@wellandlighthouse.com>
2015-03-04 15:41:06 -0500

    Quick Package bash script to save some time

---
+++ package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Preps and compiles the S3FS Package
+# Written by louie@wellandlighthouse.com
+# S3FS-Fuse released under GNU at https://github.com/s3fs-fuse/s3fs-fuse
+VER="1.78"
+
+# Move / update files for source tarball
+mkdir s3fs-fuse_$VER
+cd s3fs-fuse_$VER
+cp ../doc/ . -R
+cp ../src/ . -R
+cp ../test/ . -R
+cp ../autogen.sh .
+cp ../configure.ac .
+cp ../Makefile.am .
+tar -czf ../s3fs-fuse_$VER.tar.gz ../s3fs-fuse_$VER/
+
+# Set up to build Package
+cp ../debian/ . -R
+debuild -us -uc
+
+# Clean up after yourself
+cd ..
+rm -rf s3fs-fuse_$VER/
